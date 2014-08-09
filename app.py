5d0581dd3b95ad390a1e4821e29236b387827ca1
Ngo Duy Khanh <khanh@trobz.com>
2014-08-09 14:09:19 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,145 @@
+#!flask/bin/python
+
+import os
+import PIL
+from PIL import Image
+import simplejson
+import traceback
+
+from flask import Flask, request, render_template, session, redirect, url_for, flash, send_from_directory
+from flask.ext.bootstrap import Bootstrap
+from werkzeug import secure_filename
+
+from lib.upload_file import uploadfile
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['UPLOAD_FOLDER'] = 'data/'
+app.config['THUMBNAIL_FOLDER'] = 'data/thumbnail/'
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['txt', 'gif', 'png', 'jpg', 'jpeg', 'bmp', 'rar', 'zip', '7zip', 'doc', 'docx'])
+IGNORED_FILES = set(['.gitignore'])
+
+bootstrap = Bootstrap(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def gen_file_name(filename):
+    """
+    If file was exist already, rename it and return a new name
+    """
+
+    i = 1
+    while os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+        name, extension = os.path.splitext(filename)
+        filename = '%s_%s%s' % (name, str(i), extension)
+        i = i + 1
+
+    return filename
+
+
+def create_thumbnai(image):
+    try:
+        basewidth = 80
+        img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], image))
+        wpercent = (basewidth/float(img.size[0]))
+        hsize = int((float(img.size[1])*float(wpercent)))
+        img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+        img.save(os.path.join(app.config['THUMBNAIL_FOLDER'], image))
+
+        return True
+    
+    except:
+        print traceback.format_exc()
+        return False
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        #pprint (vars(objectvalue))
+
+        if file:
+            filename = secure_filename(file.filename)
+            filename = gen_file_name(filename)
+            mimetype = file.content_type
+
+
+            if not allowed_file(file.filename):
+                result = uploadfile(name=filename, type=mimetype, size=0, not_allowed_msg="Filetype not allowed")
+
+            else:
+                # save file to disk
+                uploaded_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(uploaded_file_path)
+
+                # create thumbnail after saving
+                if mimetype.startswith('image'):
+                    create_thumbnai(filename)
+                
+                # get file size after saving
+                size = os.path.getsize(uploaded_file_path)
+
+                # return json for js call back
+                result = uploadfile(name=filename, type=mimetype, size=size)
+            
+            return simplejson.dumps({"files": [result.get_file()]})
+
+    if request.method == 'GET':
+        # get all file in ./data directory
+        files = [ f for f in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'],f)) and f not in IGNORED_FILES ]
+        
+        file_display = []
+
+        for f in files:
+            size = os.path.getsize(os.path.join(app.config['UPLOAD_FOLDER'], f))
+            file_saved = uploadfile(name=f, size=size)
+            file_display.append(file_saved.get_file())
+
+        return simplejson.dumps({"files": file_display})
+
+    return redirect(url_for('index'))
+
+
+@app.route("/delete/<string:filename>", methods=['DELETE'])
+def delete(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file_thumb_path = os.path.join(app.config['THUMBNAIL_FOLDER'], filename)
+
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)
+
+            if os.path.exists(file_thumb_path):
+                os.remove(file_thumb_path)
+            
+            return simplejson.dumps({filename: 'True'})
+        except:
+            return simplejson.dumps({filename: 'False'})
+
+
+# serve static files
+@app.route("/thumbnail/<string:filename>", methods=['GET'])
+def get_thumbnail(filename):
+    return send_from_directory(os.path.join(app.config['THUMBNAIL_FOLDER']), filename=filename)
+
+
+@app.route("/data/<string:filename>", methods=['GET'])
+def get_file(filename):
+    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER']), filename=filename)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port=9191)
---
+++ lib/upload_file.py
@@ -0,0 +1,67 @@
+import os
+
+class uploadfile():
+    def __init__(self, name, type=None, size=None, not_allowed_msg=''):
+        self.name = name
+        self.type = type
+        self.size = size
+        self.not_allowed_msg = not_allowed_msg
+        self.url = "data/%s" % name
+        self.thumbnail_url = "thumbnail/%s" % name
+        self.delete_url = "delete/%s" % name
+        self.delete_type = "DELETE"
+
+
+    def is_image(self):
+        fileName, fileExtension = os.path.splitext(self.name.lower())
+
+        if fileExtension in ['.jpg', '.png', '.jpeg', '.bmp']:
+            return True
+
+        return False
+
+
+    def get_file(self):
+        if self.type != None:
+            # POST an image
+            if self.type.startswith('image'):
+                return {"name": self.name,
+                        "type": self.type,
+                        "size": self.size, 
+                        "url": self.url, 
+                        "thumbnailUrl": self.thumbnail_url,
+                        "deleteUrl": self.delete_url, 
+                        "deleteType": self.delete_type,}
+            
+            # POST an normal file
+            elif self.not_allowed_msg == '':
+                return {"name": self.name,
+                        "type": self.type,
+                        "size": self.size, 
+                        "url": self.url, 
+                        "deleteUrl": self.delete_url, 
+                        "deleteType": self.delete_type,}
+
+            # File type is not allowed
+            else:
+                return {"error": self.not_allowed_msg,
+                        "name": self.name,
+                        "type": self.type,
+                        "size": self.size,}
+
+        # GET image from disk
+        elif self.is_image():
+            return {"name": self.name,
+                    "size": self.size, 
+                    "url": self.url, 
+                    "thumbnailUrl": self.thumbnail_url,
+                    "deleteUrl": self.delete_url, 
+                    "deleteType": self.delete_type,}
+        
+        # GET normal file from disk
+        else:
+            return {"name": self.name,
+                    "size": self.size, 
+                    "url": self.url, 
+                    "deleteUrl": self.delete_url, 
+                    "deleteType": self.delete_type,}
