8695ffa11c0537d6048be35aa6dd3ff556538af7
anderson <lhongda@live.com>
2018-03-26 03:08:53 +0800

    Finish wifi page.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
+import Vant from 'vant';
+import 'vant/lib/vant-css/index.css';
 import App from './App';
 import router from './router';
 
-
+Vue.use(Vant);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
--- src/views/Wifi/Wifi.vue
+++ src/views/Wifi/Wifi.vue
@@ -14,6 +14,7 @@
 
     <h1 class="text-center">Cube Wifi Manage</h1>
     <h3 class="text-center">对工作在2.4Ghz 5Ghz设备进行安全风险检测</h3>
+
     <b-container>
       <b-row align-h="between">
         <b-col cols="4">
@@ -24,10 +25,33 @@
         </b-col>
       </b-row>
     </b-container>
+
+    <b-table :items="items" :fields="fields">
+      <div slot="JAM" slot-scope="data">
+        <van-switch v-model="items[data.index].JAM" @change="onSwitch" />
+      </div>
+    </b-table>
+
+    <b-container>
+      <b-row align-h="between">
+        <b-col cols="4">
+          <h4>Client List</h4>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-table :items="items2" :fields="fields2">
+      <div slot="JAM" slot-scope="data">
+        <van-switch v-model="items2[data.index].JAM" @change="onSwitch" />
+      </div>
+    </b-table>
+
   </div>
 </template>
 
 <script>
+  /* eslint-disable no-restricted-syntax */
+
   import CubeNav from '@/components/CubeNav';
 
   export default {
@@ -38,7 +62,31 @@
     data() {
       return {
         showDismissibleAlert: true,
+        test: [true],
+        fields: ['SSID', 'BSSID', 'RSSI', 'JAM'],
+        items: [
+        { Index: 0, SSID: '360WIFI-XX', BSSID: '11:22:33:44:55:66', RSSI: '-80', JAM: false },
+        { Index: 1, SSID: '360WIFI-X2', BSSID: '11:22:33:44:55:66', RSSI: '-94', JAM: false },
+        { Index: 2, SSID: '360WIFI-X3', BSSID: '11:22:33:44:55:66', RSSI: '-81', JAM: false },
+        ],
+        fields2: ['NAME', 'MAC', 'Data', 'JAM'],
+        items2: [
+          { Index: 0, NAME: 'iPhone', MAC: '11:22:33:44:55:66', Data: '522', JAM: false },
+          { Index: 1, NAME: 'Android', MAC: '11:22:33:44:55:66', Data: '94', JAM: false },
+          { Index: 2, NAME: 'iPhone', MAC: '11:22:33:44:55:66', Data: '101', JAM: false },
+        ],
       };
+    },
+    methods: {
+      onSwitch() {
+        // TODO: Check which item's JAM is true and do more thing.
+        // TODO: Check is wifi list or client list's item
+        for (const item of this.items) {
+          if (item.JAM === true) {
+            console.log(item.Index);
+          }
+        }
+      },
     },
   };
 </script>
