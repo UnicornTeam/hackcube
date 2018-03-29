97d25a60c069ce45e8f656adf24d7a3a4e924033
anderson <lhongda@live.com>
2018-03-29 12:22:45 +0800

    Add channel select.

--- src/views/Wifi/Wifi.vue
+++ src/views/Wifi/Wifi.vue
@@ -6,11 +6,17 @@
     <h3 class="text-center">对工作在2.4Ghz 5Ghz设备进行安全风险检测</h3>
     <br/>
     <b-container>
-      <b-row align-h="between">
+      <b-row >
         <b-col cols="4">
           <h5>WiFi List</h5>
         </b-col>
-        <b-col cols="2">
+        <b-col cols="1"></b-col>
+        <b-col cols="4" style="padding-right: 4px;">
+          <Select v-model="channel" size="small" placeholder="Channel" filterable>
+            <Option v-for="item in channelList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+        </b-col>
+        <b-col cols="1">
           <b-button size="sm" variant="success" @click="onClickScan">Scan</b-button>
         </b-col>
       </b-row>
@@ -36,95 +42,186 @@
 </template>
 
 <script>
-  /* eslint-disable no-restricted-syntax */
+/* eslint-disable no-restricted-syntax */
 
-  import CubeNav from '@/components/CubeNav';
-  import axios from 'axios';
+import CubeNav from '@/components/CubeNav';
+import axios from 'axios';
 
-  export default {
-    name: 'Wifi',
-    components: {
-      CubeNav,
+export default {
+  name: 'Wifi',
+  components: {
+    CubeNav,
+  },
+  data() {
+    return {
+      scanStatus: 'off',
+      fields: ['SSID', 'BSSID', 'RSSI', 'JAM'],
+      items: [
+        {
+          Index: 0,
+          SSID: '360WIFI-XX',
+          BSSID: '11:22:33:44:55:66',
+          RSSI: '-80',
+          JAM: false,
+        },
+        {
+          Index: 1,
+          SSID: '360WIFI-X2',
+          BSSID: '11:22:33:44:55:66',
+          RSSI: '-94',
+          JAM: false,
+        },
+        {
+          Index: 2,
+          SSID: '360WIFI-X3',
+          BSSID: '11:22:33:44:55:66',
+          RSSI: '-81',
+          JAM: false,
+        },
+      ],
+      fields2: ['NAME', 'MAC', 'RSSI', 'JAM'],
+      items2: [
+        {
+          Index: 0,
+          NAME: 'iPhone',
+          MAC: '11:22:33:44:55:66',
+          RSSI: '522',
+          JAM: false,
+        },
+        {
+          Index: 1,
+          NAME: 'Android',
+          MAC: '11:22:33:44:55:66',
+          RSSI: '94',
+          JAM: false,
+        },
+        {
+          Index: 2,
+          NAME: 'iPhone',
+          MAC: '11:22:33:44:55:66',
+          RSSI: '101',
+          JAM: false,
+        },
+      ],
+      channel: '',
+      channelList: [],
+    };
+  },
+  methods: {
+    onSwitch(api, value, index, isOpen) {
+      console.log(api, value, index, isOpen);
+      const action = isOpen ? 'on' : 'off';
+      axios
+        .get(`${process.env.BACKEND_HOST}/${api}/${value}/${action}`)
+        .then((response) => {
+          console.log(response.data);
+          const result = response.data;
+          this.$Message.success(result.message);
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$Message.error('Call process fail');
+        });
     },
-    data() {
-      return {
-        scanStatus: 'off',
-        fields: ['SSID', 'BSSID', 'RSSI', 'JAM'],
-        items: [
-        { Index: 0, SSID: '360WIFI-XX', BSSID: '11:22:33:44:55:66', RSSI: '-80', JAM: false },
-        { Index: 1, SSID: '360WIFI-X2', BSSID: '11:22:33:44:55:66', RSSI: '-94', JAM: false },
-        { Index: 2, SSID: '360WIFI-X3', BSSID: '11:22:33:44:55:66', RSSI: '-81', JAM: false },
-        ],
-        fields2: ['NAME', 'MAC', 'RSSI', 'JAM'],
-        items2: [
-          { Index: 0, NAME: 'iPhone', MAC: '11:22:33:44:55:66', RSSI: '522', JAM: false },
-          { Index: 1, NAME: 'Android', MAC: '11:22:33:44:55:66', RSSI: '94', JAM: false },
-          { Index: 2, NAME: 'iPhone', MAC: '11:22:33:44:55:66', RSSI: '101', JAM: false },
-        ],
-      };
+    onClickScan() {
+      // send request with action to backend
+      this.scanStatus = this.scanStatus === 'on' ? 'off' : 'on';
+      axios
+        .get(`${process.env.BACKEND_HOST}/wifi_scan/${this.scanStatus}`)
+        .then((response) => {
+          const result = response.data;
+          console.log(result);
+          this.$Message.success(result.message);
+          if (this.scanStatus === 'on') {
+            this.$timer.start('fetchWifiList');
+          } else if (this.scanStatus === 'off') {
+            this.$timer.stop('fetchWifiList');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$Message.error(err);
+        });
     },
-    methods: {
-      onSwitch(api, value, index, isOpen) {
-        console.log(api, value, index, isOpen);
-        const action = isOpen ? 'on' : 'off';
-        axios.get(`${process.env.BACKEND_HOST}/${api}/${value}/${action}`)
-          .then((response) => {
-            console.log(response.data);
-            const result = response.data;
-            this.$Message.success(result.message);
-          })
-          .catch((err) => {
-            console.log(err);
-            this.$Message.error('Call process fail');
-          });
-      },
-      onClickScan() {
-        // send request with action to backend
-        this.scanStatus = this.scanStatus === 'on' ? 'off' : 'on';
-        axios.get(`${process.env.BACKEND_HOST}/wifi_scan/${this.scanStatus}`)
+    fetchWifiList() {
+      if (this.scanStatus === 'on') {
+        axios
+          .get(`${process.env.BACKEND_HOST}/ap_list`)
           .then((response) => {
             const result = response.data;
             console.log(result);
-            this.$Message.success(result.message);
-            if (this.scanStatus === 'on') {
-              this.$timer.start('fetchWifiList');
-            } else if (this.scanStatus === 'off') {
-              this.$timer.stop('fetchWifiList');
-            }
+            // this.$Message.success('Fetch wifi list success.');
+            this.items = result.ap_list;
           })
           .catch((err) => {
             console.log(err);
-            this.$Message.error(err);
+            this.$Message.error('Fetch wifi list fail.');
           });
-      },
-      fetchWifiList() {
-        if (this.scanStatus === 'on') {
-          axios.get(`${process.env.BACKEND_HOST}/ap_list`)
-            .then((response) => {
-              const result = response.data;
-              console.log(result);
-              // this.$Message.success('Fetch wifi list success.');
-              this.items = result.ap_list;
-            })
-            .catch((err) => {
-              console.log(err);
-              this.$Message.error('Fetch wifi list fail.');
-            });
-        }
-      },
+      }
     },
-    created() {
-      // TODO: 初始化数据
-      // TODO: Save the final fresh wifi list when leave page
-      // TODO: And restore it when back to this page.
-      console.log(`items is: ${this.items}`);
+    getChannelList() {
+      const channelList = [];
+      const numList = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        36,
+        40,
+        44,
+        48,
+        52,
+        56,
+        60,
+        64,
+        100,
+        104,
+        108,
+        112,
+        115,
+        120,
+        124,
+        128,
+        132,
+        136,
+        140,
+        149,
+        153,
+        157,
+        161,
+        165,
+      ];
+      for (const num of numList) {
+        const channel = {
+          value: num,
+          label: num,
+        };
+        channelList.push(channel);
+      }
+      this.channelList = channelList;
     },
-    timers: {
-      fetchWifiList: { time: 3000, autostart: false, repeat: true },
-    },
-  };
+  },
+  created() {
+    // TODO: 初始化数据
+    // TODO: Save the final fresh wifi list when leave page
+    // TODO: And restore it when back to this page.
+    console.log(`items is: ${this.items}`);
+    this.getChannelList();
+  },
+  timers: {
+    fetchWifiList: { time: 3000, autostart: false, repeat: true },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
