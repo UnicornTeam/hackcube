d96a0e9af461124cdeb790d4df72acc180e92956
anderson <lhongda@live.com>
2018-04-04 13:55:47 +0800

    [TESTING]First nightwatch test.

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,9 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.board')
+      .assert.containsText('h1', 'Cube Wifi Manage')
+      .assert.elementCount('img', 0)
       .end();
   },
 };
